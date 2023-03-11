a3a8682ad36d19d386954ea529027a14f80ff8d3
xiao_wine <xiaowine0@gmailc.om>
2023-03-11 21:12:02 +0800

    添加 强制使用新版HD图标

--- app/src/main/java/com/yuk/miuiXXL/activity/pages/SystemUIPage.kt
+++ app/src/main/java/com/yuk/miuiXXL/activity/pages/SystemUIPage.kt
@@ -49,6 +49,11 @@ class SystemUIPage : BasePage() {
                 textId = R.string.systemui_wave_charge_animation, tipsId = R.string.systemui_wave_charge_animation_summary
             ), SwitchV("systemui_wave_charge_animation", false)
         )
+        TextSummaryWithSwitch(
+            TextSummaryV(
+                textId = R.string.systemui_use_new_hd, tipsId = R.string.systemui_use_new_summary
+            ), SwitchV("systemui_use_new_hd", false)
+        )
     }
 
 }
